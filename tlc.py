4a28b7e96a28d3760ff9a185f3bf8d0f946f2210
Son Nguyen <135491130+ngmsonn@users.noreply.github.com>
2024-02-07 15:49:43 +0700

    Update tlc.py

--- tlc.py
+++ tlc.py
@@ -1,62 +1,79 @@
-# Phiên bản 2.0
+# Version 3.0
+# Author: Son Nguyen Manh
+# Github: https://github.com/ngmsonn
+# Website: https://ngmsonn.github.io/
 
 from PIL import Image
+from scipy.signal import savgol_filter
 import matplotlib.pyplot as plt 
 import numpy as np 
 import cv2
 import os 
 import math
 
-def tlc2spec(input_img, background):
-    """_summary_
+def tlc2peaks(input_img, background):
+    """ Create peaks from TLC image.
 
     Args:
         input_img: Path to TLC image
-        background: Choose color background of TLC image. W (White), B (Black), G (Green), BL (Blue)
+        background: Choose color background of TLC image. W (White), B (Black), G (Green) and BL (Blue)
     """
     org_img = cv2.imread(input_img)
-    rgb_img = cv2.cvtColor(org_img, cv2.COLOR_BGR2RGB)
+    rbg_img = cv2.cvtColor(org_img, cv2.COLOR_BGR2RGB)
     axis_x = []
     axis_y = []
-    if background == "W":
-        for i in range(rgb_img.shape[1]):
-            axis_x.append(i)
-        for j in range(int(rgb_img.shape[1])):
-            R = int(rgb_img[int((rgb_img.shape[0])/2), j][0])
-            G = int(rgb_img[int((rgb_img.shape[0])/2), j][1])
-            B = int(rgb_img[int((rgb_img.shape[0])/2), j][2])
-            I = 1/((R+G+B)/3)
+    
+    # White and green color background of TLC image
+    if background == "W" or "G":
+        mean_RGB = []
+        for i in range(rbg_img.shape[0]):
+            for k in range(rbg_img.shape[1]):
+                mean_RGB.append(rbg_img[i, k].mean())
+        max_mean_rgb = max(mean_RGB)
+        for h in range(int(rbg_img.shape[1])):
+            axis_x.append(h)
+        for j in range(int(rbg_img.shape[1])):
+            R = int(rbg_img[int((rbg_img.shape[0]/2)), j][0])
+            G = int(rbg_img[int((rbg_img.shape[0]/2)), j][1])
+            B = int(rbg_img[int((rbg_img.shape[0]/2)), j][2])
+            I = int(max_mean_rgb-(R+G+B)/3)
             axis_y = np.append(axis_y, I)
-    elif background == "G":
-        for i in range(rgb_img.shape[1]):
-            axis_x.append(i)
-        for j in range(int(rgb_img.shape[1])):
-            R = int(rgb_img[int((rgb_img.shape[0])/2), j][0])
-            G = int(rgb_img[int((rgb_img.shape[0])/2), j][1])
-            B = int(rgb_img[int((rgb_img.shape[0])/2), j][2])
-            I = 1/((R+G+B)/3)
-            axis_y = np.append(axis_y, I)
-    elif background == "B":
-        for i in range(rgb_img.shape[1]):
-            axis_x.append(i)
-        for j in range(int(rgb_img.shape[1])):
-            R = int(rgb_img[int((rgb_img.shape[0])/2), j][0])
-            G = int(rgb_img[int((rgb_img.shape[0])/2), j][1])
-            B = int(rgb_img[int((rgb_img.shape[0])/2), j][2])
-            I = ((R+G+B)/3)
-            axis_y = np.append(axis_y, I)
-    elif background == "BL":
-        for i in range(rgb_img.shape[1]):
-            axis_x.append(i)
-        for j in range(int(rgb_img.shape[1])):
-            R = int(rgb_img[int((rgb_img.shape[0])/2), j][0])
-            G = int(rgb_img[int((rgb_img.shape[0])/2), j][1])
-            B = int(rgb_img[int((rgb_img.shape[0])/2), j][2])
-            I = ((R+G+B)/3)
+    elif background == "BL" or "B":
+        for h in range(int(rbg_img.shape[1])):
+            axis_x.append(h)
+        for j in range(int(rbg_img.shape[1])):
+            R = int(rbg_img[int((rbg_img.shape[0]/2)), j][0])
+            G = int(rbg_img[int((rbg_img.shape[0]/2)), j][1])
+            B = int(rbg_img[int((rbg_img.shape[0]/2)), j][2])
+            I = int((R+G+B)/3)
             axis_y = np.append(axis_y, I)
     else:
-        print("Color background is not valiable.")
+        print("Error!!!")
     return plt.plot(axis_x, axis_y)
+
+def get_RGB(input_img):
+    """Get RGB value of each pixel
     
-        
+    Args:
+        input_img: Path to image.
+    """
+    org_img = cv2.imread(input_img)
+    rbg_img = cv2.cvtColor(org_img, cv2.COLOR_BGR2RGB)
+    mean_RGB = []
+    for i in range(rbg_img.shape[0]):
+            for k in range(rbg_img.shape[1]):
+                mean_RGB.append(rbg_img[i, k].mean())
+    return np.array(mean_RGB)
+
+def get_RGB_row(input_img, pxrow):
+    org_img = cv2.imread(input_img)
+    rbg_img = cv2.cvtColor(org_img, cv2.COLOR_BGR2RGB)
+    mean_RGB_row = []
+    for j in range(int(rbg_img.shape[1])):
+            R = int(rbg_img[int((pxrow)), j][0])
+            G = int(rbg_img[int((pxrow)), j][1])
+            B = int(rbg_img[int((pxrow)), j][2])
+            I = int((R+G+B)/3)
+            mean_RGB_row = np.append(mean_RGB_row, I)
+    return np.array(mean_RGB_row)
     
